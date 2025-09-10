ea26bfaf8728fb2e685778ceb733e8d893c3e492
Jhon lloyd Cape <epacxzs@gmail.com>
2025-09-10 23:29:53 +0800

    Initial commit: Nutrilife meal planning app

---
+++ src/main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import base64
+
+st.set_page_config(page_title="Nutrilife", layout="wide")
+
+
+# ---------- LOAD THE HOMEPAGE IMAGE ----------
+
+def homepage_image(image_file):
+    with open(image_file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+image = homepage_image("assets/plate.png")
+adobo = homepage_image("assets/adobo.png")
+saging = homepage_image("assets/saging.png")
+tinola = homepage_image("assets/tinola.png")
+
+
+
+# ---------- LOAD THE CSS ----------
+
+def homepage_css(file_css):
+    with open(file_css) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+homepage_css("style/homepage.css")
+
+
+# ------------ LOAD HTML -------------
+def homepage_html(file_html, **kwargs):
+    with open(file_html, "r") as f:
+        html = f.read()
+    if kwargs:
+        html = html.format(**kwargs)
+    st.markdown(html, unsafe_allow_html=True)
+
+homepage_html("html/homepage.html", image=image, adobo=adobo, saging=saging, tinola=tinola)
